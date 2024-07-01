985776844e89fc7687d0e93aaa10e2a7494f2c17
CJ Bunyi <cjb.nyi@gmail.com>
2024-07-02 01:09:49 +0800

    Finished DFS and BFS

--- src/main.c
+++ src/main.c
@@ -1,35 +1,63 @@
 #include <stdio.h>
+#include <string.h>
 #include "graph.h"
 #include "queue.h"
 #include "stack.h"
 #include "traversal.h"
 
-#define INPUT_FILENAME "GRAPH.txt"
+#define TEST_INPUT_FILENAME "..\\graphs\\GRAPH2.txt"
 
 
 int main() {
     
     // program variables
-    String31 filename, startingVertex;
+    String31 directory = "..\\graphs\\";
+    String31 filename;
     int errorCode;
-    Graph* graph;
-    
+    AdjList* currVertex;
+    String31 startingVertexID;
+    Graph* graph = createGraph();
+    FILE *fp;
+
     /* ask the user for the filename */
-    printf("Input filename: ");
-    scanf(" %s", filename);
+    // ### printf("Input filename: ");
+    // ### scanf(" %s", filename);
 
     // TODO: Address the error code. Refer to MCO1.
-    /* construct the undirected graph*/
-    errorCode = constructGraph(filename, &graph);
+    /* construct the undirected graph */
+    // ### strcat(directory, filename);
+    errorCode = constructGraph(TEST_INPUT_FILENAME, graph);
+    if (errorCode == -1)
+        printf("File not found.\n");
     // insert error handling code here
+
+    fp = fopen(OUTPUT_FILENAME, "w");
+    if (fp == NULL)
+        printf("Output file failed to be generated.\n");
+
+    // temporary; will be refactored elsewhere
+    currVertex = graph->firstAdjList;
+    while (currVertex != NULL) {
+        printf("\n%-15s%d", currVertex->vertexID, currVertex->degree);
+        fprintf(fp, "\n%-15s%d", currVertex->vertexID, currVertex->degree);
+        currVertex = currVertex->nextAdjList;
+        // ### printf("Here 4\n");
+    }
+    printf("\n");
+    // ### printf("Here 5\n");
+    fclose(fp);
 
     /* ask the user for the starting vertex */
     printf("Input starting vertex for the traversal: ");
-    scanf(" %s", startingVertex);
+    scanf(" %s", startingVertexID);
 
     /* execute the chosen traversal method and produce the output file */
-    breadthFirstSearch(graph, startingVertex);
-    depthFirstSearch(graph, startingVertex);
+    breadthFirstSearch(graph, startingVertexID);
+    printf("Finished BFS!\n");
+    depthFirstSearch(graph, startingVertexID);
+    printf("Finished DFS!\n");
 
+    // deleteGraph(graph);
+    printf("Reached end of program!\n");
     return 0;
 }
